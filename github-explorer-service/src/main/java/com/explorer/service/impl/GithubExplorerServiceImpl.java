da76e5ecb3174d184fb2480104fbf48dabf2bf5e
Marcin Mikołajczyk <marcin.mikolajczyk22@gmail.com>
2023-09-09 21:29:34 +0200

    #5 Added exception hadler

--- github-explorer-service/src/main/java/com/explorer/service/impl/GithubExplorerServiceImpl.java
+++ github-explorer-service/src/main/java/com/explorer/service/impl/GithubExplorerServiceImpl.java
@@ -1,12 +1,11 @@
 package com.explorer.service.impl;
 
+import com.explorer.infrastructure.utils.UtilsObjectMapper;
 import com.explorer.dto.Branch;
 import com.explorer.dto.Repository;
+import com.explorer.infrastructure.exceptions.ResponseException;
 import com.explorer.model.*;
 import com.explorer.service.GithubExplorerService;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -26,28 +25,25 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class GithubExplorerServiceImpl implements GithubExplorerService {
     @Value("${github.api.address}")
     String gitHubApiAddress;
-    private final Gson gson;
 
     @Override
     public List<GitHubRepository> receiveGithubRepositories(String username) {
-        String preparedUri = buildUri(MessageFormat.format("{0}/users/{1}/repos",
-                gitHubApiAddress, username), RepositoryType.OWNER, SortType.CREATED, SortDirectionType.ASC, 1, 30);
-        List<Repository> repositories = gson.fromJson(invokeWebClient(HttpMethod.GET, preparedUri), new TypeToken<List<Repository>>(){}.getType());
+        String preparedUri = buildUri(MessageFormat.format("{0}/users/{1}/repos", gitHubApiAddress, username),
+                RepositoryType.OWNER, SortType.CREATED, SortDirectionType.ASC, 1, 30);
+        List<Repository> repositories = UtilsObjectMapper.getInstanceAsList(invokeWebClient(HttpMethod.GET, preparedUri), Repository.class);
         return repositories.stream()
                 .filter(Objects::nonNull)
                 .filter(repo -> !repo.fork())
-                .map(repo -> prepareGitHubRepository(repo.name(), repo.owner().login(),
-                        processBranches(repo.branchesUrl())))
+                .map(repo -> prepareGitHubRepository(repo.name(), repo.owner().login(), processBranches(repo.branchesUrl())))
                 .collect(Collectors.toList());
     }
 
     private List<GithubBranch> processBranches(String branchUrl){
-        String result = invokeWebClient(HttpMethod.GET, branchUrl.replaceAll("\\{\\/branch\\}", ""));
-        List<Branch> branches = gson.fromJson(result, new TypeToken<List<Branch>>(){}.getType());
+        String responseAsJson = invokeWebClient(HttpMethod.GET, branchUrl.replaceAll("\\{\\/branch\\}", ""));
+        List<Branch> branches = UtilsObjectMapper.getInstanceAsList(responseAsJson, Branch.class);
         return branches.stream()
                 .filter(Objects::nonNull)
                 .map(branch -> new GithubBranch(branch.name(), branch.commit().sha()))
@@ -55,24 +51,25 @@ public class GithubExplorerServiceImpl implements GithubExplorerService {
     }
 
     private String invokeWebClient(HttpMethod method, String uri){
-        log.info("Invoke web client for: {}", uri);
+        log.info("Invoked web client for: {}", uri);
         return WebClient.create()
                 .method(method)
                 .uri(uri)
                 .accept(MediaType.APPLICATION_JSON)
-                .exchangeToMono(clientResponse -> onExchangeMono(clientResponse))
-                .timeout(Duration.ofSeconds(20))
+                .exchangeToMono(this::onExchangeMono)
                 .doOnError(ex -> log.error(ex.getMessage()))
+                .timeout(Duration.ofSeconds(20))
                 .retry(3)
                 .block();
     }
 
     private Mono<String> onExchangeMono(ClientResponse response){
-        return response.statusCode().is2xxSuccessful()
-                ? response.bodyToMono(String.class)
-                : response.statusCode().is4xxClientError()
-                  ? Mono.just("Error response for WebClient")
-                  : response.createException().flatMap(Mono::error);
+        if (response.statusCode().is2xxSuccessful())
+            return response.bodyToMono(String.class);
+        else if (response.statusCode().value() == 404)
+            throw new ResponseException("GitHub username not found !");
+         else
+            throw new ResponseException("Incorrect response from server !");
     }
 
     private String buildUri(String baseUrl, RepositoryType repositoryType, SortType sortType,
